3f1a80ed55544264876df111765ec17c982b9c9a
Kirill Golodnov <golodnov.kirill@gmail.com>
2013-07-17 00:44:07 +0300

    One small task from facebook interview from CareerCup.

---
+++ CareerCup/src/main/java/com/github/holodnov/careercup/LeastDifferenceInArrayWithGivenNumber.java
@@ -0,0 +1,39 @@
+package com.github.holodnov.careercup;
+
+/**
+ * @see <a
+ *      href="http://www.careercup.com/question?id=20982670">http://www.careercup.com/question?id=20982670</a>
+ * 
+ * @author Kyrylo Holodnov
+ */
+public class LeastDifferenceInArrayWithGivenNumber {
+    public static int getElementWithLeastDifference(int[] sequence, int element) {
+	if ((sequence == null) || (sequence.length == 0)) {
+	    throw new IllegalArgumentException("Input array is null or empty");
+	}
+	int left = 0;
+	int right = sequence.length - 1;
+	while (left < right) {
+	    int median = left + ((right - left) >> 1);
+	    if (sequence[median] < element) {
+		left = median + 1;
+	    } else {
+		right = median;
+	    }
+	}
+	if (sequence[left] == element) {
+	    return element;
+	} else {
+	    if (left == 0) {
+		return sequence[0];
+	    } else {
+		if (Math.abs(element - sequence[left]) >= Math.abs(element
+			- sequence[left - 1])) {
+		    return sequence[left - 1];
+		} else {
+		    return sequence[left];
+		}
+	    }
+	}
+    }
+}
---
+++ CareerCup/src/test/java/com/github/holodnov/careercup/LeastDifferenceInArrayWithGivenNumberTest.java
@@ -0,0 +1,60 @@
+package com.github.holodnov.careercup;
+
+import static com.github.holodnov.careercup.LeastDifferenceInArrayWithGivenNumber.getElementWithLeastDifference;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @see <a
+ *      href="http://www.careercup.com/question?id=20982670">http://www.careercup.com/question?id=20982670</a>
+ * 
+ * @author Kyrylo Holodnov
+ */
+public class LeastDifferenceInArrayWithGivenNumberTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetElementWithLeastDifferenceNullArray() {
+	getElementWithLeastDifference(null, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetElementWithLeastDifferenceEmptyArray() {
+	getElementWithLeastDifference(new int[0], 1);
+    }
+
+    @Test
+    public void testGetElementWithLeastDifference1() {
+	int res = getElementWithLeastDifference(new int[] { 1, 3, 5, 7, 9, 11,
+		13, 15 }, 1);
+	assertThat("Unexpected result", res, is(1));
+    }
+
+    @Test
+    public void testGetElementWithLeastDifference2() {
+	int res = getElementWithLeastDifference(new int[] { 1, 4, 5, 7, 9, 11,
+		13, 15 }, 2);
+	assertThat("Unexpected result", res, is(1));
+    }
+
+    @Test
+    public void testGetElementWithLeastDifference3() {
+	int res = getElementWithLeastDifference(new int[] { 1, 3, 5, 7, 9, 11,
+		13, 15 }, 2);
+	assertThat("Unexpected result", res, is(1));
+    }
+
+    @Test
+    public void testGetElementWithLeastDifference4() {
+	int res = getElementWithLeastDifference(new int[] { 1, 4, 5, 7, 9, 11,
+		13, 15 }, 2);
+	assertThat("Unexpected result", res, is(1));
+    }
+
+    @Test
+    public void testGetElementWithLeastDifference5() {
+	int res = getElementWithLeastDifference(new int[] { 1, 4, 5, 7, 9, 11,
+		13, 15 }, 5);
+	assertThat("Unexpected result", res, is(5));
+    }
+}
