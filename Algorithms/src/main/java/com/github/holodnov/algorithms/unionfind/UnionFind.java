ea3c73433d9d054d9ac85b5c1a2b4d900ccb9073
Kirill Golodnov <golodnov.kirill@gmail.com>
2014-08-25 23:16:26 +0300

    Added union-find data structure

---
+++ Algorithms/src/main/java/com/github/holodnov/algorithms/unionfind/UnionFind.java
@@ -0,0 +1,96 @@
+package com.github.holodnov.algorithms.unionfind;
+
+/**
+ * A <tt>UnionFind</tt> is an implementation of union-find data structure. The
+ * amortized time complexity for <b>union</b> and <b>find</b> operations has
+ * inverse Ackermann complexity.
+ *
+ * @see <a
+ * href="http://en.wikipedia.org/wiki/Disjoint-set_data_structure">http://en.wikipedia.org/wiki/Disjoint-set_data_structure</a>
+ * @author Kyrylo Holodnov
+ */
+public class UnionFind {
+
+    private final int n;
+    private final int[] roots;
+    private final int[] ranks;
+
+    private int subsetsCount;
+
+    /**
+     * Creates union-find data structure with <b>n</b> elements and each element
+     * in its own subset.
+     *
+     * @param n input number of elements
+     * @throws IllegalArgumentException if input parameter is not positive
+     */
+    public UnionFind(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Initial number of components "
+                    + "should be positive");
+        }
+        this.n = n;
+        subsetsCount = n;
+        roots = new int[n];
+        ranks = new int[n];
+        for (int i = 0; i < roots.length; i++) {
+            roots[i] = i;
+        }
+    }
+
+    /**
+     * Determines which subset an input element is in and returns root of this
+     * subset.
+     *
+     * @param i input element
+     * @return root of subset an input element is in
+     * @throws IllegalArgumentException if input element out of bounds
+     */
+    public int find(int i) {
+        if (i < 0 || i >= n) {
+            throw new IllegalArgumentException("Input element "
+                    + "should be between 0 (inclusive) "
+                    + "and " + n + " (exclusive)");
+        }
+        while (i != roots[i]) {
+            roots[i] = roots[roots[i]];
+            i = roots[i];
+        }
+        return i;
+    }
+
+    /**
+     * Joins two subsets represented by its elements into a single subset.
+     *
+     * @param i input element from first subset
+     * @param j input element from second subset
+     * @return root of subset after joining these subsets
+     * @throws IllegalArgumentException if input elements out of bounds
+     */
+    public int union(int i, int j) {
+        int firstRoot = find(i);
+        int secondRoot = find(j);
+        if (firstRoot == secondRoot) {
+            return firstRoot;
+        }
+        if (ranks[firstRoot] < ranks[secondRoot]) {
+            roots[firstRoot] = secondRoot;
+        } else if (ranks[firstRoot] > ranks[secondRoot]) {
+            roots[secondRoot] = firstRoot;
+        } else {
+            roots[firstRoot] = secondRoot;
+            ranks[secondRoot]++;
+        }
+        subsetsCount--;
+        return roots[firstRoot];
+    }
+
+    /**
+     * Returns current number of subsets.
+     *
+     * @return number of subsets
+     */
+    public int getSubsetsCount() {
+        return subsetsCount;
+    }
+}
---
+++ Algorithms/src/test/java/com/github/holodnov/algorithms/unionfind/UnionFindTest.java
@@ -0,0 +1,59 @@
+package com.github.holodnov.algorithms.unionfind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Kyrylo Holodnov
+ */
+public class UnionFindTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindNegativeInputParam() {
+        new UnionFind(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindInputParamIsZero() {
+        new UnionFind(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindIllegalArgument1() {
+        UnionFind unionFind = new UnionFind(5);
+        unionFind.find(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindIllegalArgument2() {
+        UnionFind unionFind = new UnionFind(5);
+        unionFind.find(5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnionIllegalArgument1() {
+        UnionFind unionFind = new UnionFind(5);
+        unionFind.union(0, -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnionIllegalArgument2() {
+        UnionFind unionFind = new UnionFind(5);
+        unionFind.union(1, 5);
+    }
+
+    @Test
+    public void testUnionFindGetSubsetsCount() {
+        UnionFind unionFind = new UnionFind(5);
+        assertEquals(5, unionFind.getSubsetsCount());
+        unionFind.union(1, 2);
+        assertEquals(4, unionFind.getSubsetsCount());
+        assertTrue(unionFind.find(1) == 1 || unionFind.find(1) == 2);
+        assertTrue(unionFind.find(2) == 1 || unionFind.find(2) == 2);
+        unionFind.union(1, 4);
+        assertEquals(3, unionFind.getSubsetsCount());
+        assertTrue(unionFind.find(1) == 1 || unionFind.find(1) == 2 || unionFind.find(1) == 4);
+        assertTrue(unionFind.find(2) == 1 || unionFind.find(2) == 2 || unionFind.find(2) == 4);
+        assertTrue(unionFind.find(4) == 1 || unionFind.find(4) == 2 || unionFind.find(4) == 4);
+    }
+}
