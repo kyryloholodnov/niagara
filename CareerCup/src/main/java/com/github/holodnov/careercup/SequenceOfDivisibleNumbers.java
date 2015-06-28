d72a1a5240e5e7d28f8ff7129c5e8d06bfec4228
Kirill Golodnov <golodnov.kirill@gmail.com>
2015-06-28 19:45:09 +0300

    Small changes in task about sequence

--- CareerCup/src/main/java/com/github/holodnov/careercup/SequenceOfDivisibleNumbers.java
+++ CareerCup/src/main/java/com/github/holodnov/careercup/SequenceOfDivisibleNumbers.java
@@ -1,42 +1,29 @@
 package com.github.holodnov.careercup;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.TreeSet;
 
 /**
- * @see <a
- *      href="http://www.careercup.com/question?id=20810664">http://www.careercup.com/question?id=20810664</a>
- * 
  * @author Kyrylo Holodnov
+ * @see <a
+ * href="http://www.careercup.com/question?id=20810664">http://www.careercup.com/question?id=20810664</a>
  */
 public class SequenceOfDivisibleNumbers {
 
-    public static int getNthNumber(int n) {
-	if (n <= 0) {
-	    throw new IllegalArgumentException(
-		    "Input parameter should be positive");
-	}
-	if (n <= 5) {
-	    return n;
-	}
-	Set<Integer> numbers = new HashSet<Integer>();
-	numbers.add(1);
-	numbers.add(2);
-	numbers.add(3);
-	numbers.add(4);
-	numbers.add(5);
-	int i = 5;
-	int pos = 5;
-	while (pos < n) {
-	    i++;
-	    if (((i % 2) == 0 && numbers.contains(i / 2))
-		    || ((i % 3) == 0 && numbers.contains(i / 3))
-		    || ((i % 5) == 0 && numbers.contains(i / 5))) {
-		numbers.add(i);
-		pos++;
-	    }
-	}
-	return i;
+    public static long getNthNumber(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input parameter should be positive");
+        }
+        TreeSet<Long> set = new TreeSet<>();
+        set.add(1L);
+        long nthNumber = 1;
+        while (n > 0) {
+            nthNumber = set.ceiling(nthNumber);
+            set.remove(nthNumber);
+            set.add(2 * nthNumber);
+            set.add(3 * nthNumber);
+            set.add(5 * nthNumber);
+            n--;
+        }
+        return nthNumber;
     }
-
 }
--- CareerCup/src/test/java/com/github/holodnov/careercup/SequenceOfDivisibleNumbersTest.java
+++ CareerCup/src/test/java/com/github/holodnov/careercup/SequenceOfDivisibleNumbersTest.java
@@ -1,102 +1,100 @@
 package com.github.holodnov.careercup;
 
+import org.junit.Test;
+
 import static com.github.holodnov.careercup.SequenceOfDivisibleNumbers.getNthNumber;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
-
 /**
- * @see <a
- *      href="http://www.careercup.com/question?id=20810664">http://www.careercup.com/question?id=20810664</a>
- * 
  * @author Kyrylo Holodnov
+ * @see <a
+ * href="http://www.careercup.com/question?id=20810664">http://www.careercup.com/question?id=20810664</a>
  */
 public class SequenceOfDivisibleNumbersTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testGetNthNumberNegativeInput() {
-	getNthNumber(-1);
+        getNthNumber(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testGetNthNumberZeroInput() {
-	getNthNumber(0);
+        getNthNumber(0);
     }
 
     @Test
     public void testGetNthNumberWithOne() {
-	assertThat(getNthNumber(1), is(1));
+        assertThat(getNthNumber(1), is(1L));
     }
 
     @Test
     public void testGetNthNumberWithTwo() {
-	assertThat(getNthNumber(2), is(2));
+        assertThat(getNthNumber(2), is(2L));
     }
 
     @Test
     public void testGetNthNumberWithThree() {
-	assertThat(getNthNumber(3), is(3));
+        assertThat(getNthNumber(3), is(3L));
     }
 
     @Test
     public void testGetNthNumberWithFour() {
-	assertThat(getNthNumber(4), is(4));
+        assertThat(getNthNumber(4), is(4L));
     }
 
     @Test
     public void testGetNthNumberWithFive() {
-	assertThat(getNthNumber(5), is(5));
+        assertThat(getNthNumber(5), is(5L));
     }
 
     @Test
     public void testGetNthNumberWithSix() {
-	assertThat(getNthNumber(6), is(6));
+        assertThat(getNthNumber(6), is(6L));
     }
 
     @Test
     public void testGetNthNumberWithSeven() {
-	assertThat(getNthNumber(7), is(8));
+        assertThat(getNthNumber(7), is(8L));
     }
 
     @Test
     public void testGetNthNumberWithEight() {
-	assertThat(getNthNumber(8), is(9));
+        assertThat(getNthNumber(8), is(9L));
     }
 
     @Test
     public void testGetNthNumberWithNine() {
-	assertThat(getNthNumber(9), is(10));
+        assertThat(getNthNumber(9), is(10L));
     }
 
     @Test
     public void testGetNthNumberWithTen() {
-	assertThat(getNthNumber(10), is(12));
+        assertThat(getNthNumber(10), is(12L));
     }
 
     @Test
     public void testGetNthNumberWithEleven() {
-	assertThat(getNthNumber(11), is(15));
+        assertThat(getNthNumber(11), is(15L));
     }
 
     @Test
     public void testGetNthNumberWithTwelve() {
-	assertThat(getNthNumber(12), is(16));
+        assertThat(getNthNumber(12), is(16L));
     }
 
     @Test
     public void testGetNthNumberWithThirteen() {
-	assertThat(getNthNumber(13), is(18));
+        assertThat(getNthNumber(13), is(18L));
     }
 
     @Test
     public void testGetNthNumberWithFourteen() {
-	assertThat(getNthNumber(14), is(20));
+        assertThat(getNthNumber(14), is(20L));
     }
 
     @Test
     public void testGetNthNumberWithFifteen() {
-	assertThat(getNthNumber(15), is(24));
+        assertThat(getNthNumber(15), is(24L));
     }
-
 }
